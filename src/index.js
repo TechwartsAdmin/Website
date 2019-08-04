f5ca7bb5db03539469b45ff576733b875d0e8831
Amit <amit.jha6700@gmail.com>
2019-08-04 16:40:06 +0530

    Tlc members imgs added

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
+setInterval(() => {
+  plusSlides(1);
+}, 3000);
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
